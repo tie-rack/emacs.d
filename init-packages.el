6eac91e85e7ee6896cf5d0dcb7d0cf9f2872a58b
Chris Shea <cmshea@gmail.com>
2013-03-20 12:38:13 -0400

    Prefer melpa over marmalade

--- init-packages.el
+++ init-packages.el
@@ -5,8 +5,8 @@
     (package-install package)))
 
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
 (unless package-archive-contents
