8db2710266e3cb3edf32d2e69e3551f2643994fe
Chris Shea <cmshea@gmail.com>
2013-05-30 14:32:31 -0400

    Also set c-basic-offset to 2

--- init-whitespace.el
+++ init-whitespace.el
@@ -1,5 +1,6 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
+(setq-default c-basic-offset 2)
 (setq-default require-final-newline 't)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
