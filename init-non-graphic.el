755d2cee39ee0ab128c98d0b7e94425a0bddfe93
Chris Shea <cmshea@gmail.com>
2014-02-17 23:16:27 -0500

    Diff colors and some color rearrangement

--- init-non-graphic.el
+++ init-non-graphic.el
@@ -1,5 +1,3 @@
-;; Settings that only make sense for non-graphical emacs
-
 (require 'color-theme)
 
 (defun color-theme-shea ()
@@ -9,11 +7,18 @@
       (background-mode . dark)
       (cursor-color . "#FFFFFF")
       (foreground-color . "#FFFFFF"))
+
      (default ((t (nil))))
      (bold ((t (:bold t))))
      (italic ((t (:italic t))))
      (bold-italic ((t (:italic t :bold t))))
      (underline ((t (:underline t))))
+
+     (diff-added ((t (:foreground "#009900" :background "#000000"))))
+     (diff-removed ((t (:foreground "#FF0000" :background "#000000"))))
+     (diff-file-header ((t (:foreground "#AAAA44" :background "#222222"))))
+     (diff-hunk-header ((t (:foreground "#FFFF00" :background "#0000FF"))))
+
      (font-lock-builtin-face ((t (:foreground "#B09FD4"))))
      (font-lock-comment-face ((t (:foreground "#FF7722" :italic t))))
      (font-lock-constant-face ((t (:foreground "#AAFFBB"))))
@@ -23,14 +28,17 @@
      (font-lock-preprocessor-face ((t (:foreground "#FFFFAD"))))
      (font-lock-string-face ((t (:foreground "#FFBB99"))))
      (font-lock-type-face ((t (:foreground "white"))))
+
      (isearch ((t (:background "#003300"))))
      (lazy-highlight ((t (:background "#777700"))))
+     (region ((t (:background "#0000FF"))))
+     (secondary-selection ((t (:background "#444400"))))
+
      (minibuffer-prompt ((t (:foreground "#00FFFF"))))
      (mode-line ((t (:background "#EEEEEE" :foreground "black"))))
      (mode-line-buffer-id ((t (:background "#FFFFFF" :foreground "black" :bold t))))
      (mode-line-inactive ((t (:background "#999999" :foreground "black"))))
-     (region ((t (:background "#0000FF"))))
-     (secondary-selection ((t (:background "#444400"))))
+
      (vertical-border ((t (:foreground "white" :background "#333333")))))))
 
 (color-theme-shea)
